7b30042a566b268c383d7c1e74a4f72e90fdef91
Kyle Bennett <kylebennett00001@gmail.com>
2025-01-18 15:42:11 -0500

    chore: run linter on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,31 +21,37 @@ function App() {
   const [entriesPerPage, setEntriesPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const settingModalOpen = useSelector((state: RootState) => state.settings.settingModalOpen);
+  const settingModalOpen = useSelector(
+    (state: RootState) => state.settings.settingModalOpen
+  );
   const reposRedux = useSelector((state: RootState) => state.repo.value);
-  const selectedSort = useSelector((state: RootState) => selectedSortOption(state));
+  const selectedSort = useSelector((state: RootState) =>
+    selectedSortOption(state)
+  );
 
   const dispatch = useDispatch();
   useEffect(() => {
-    const token = localStorage.getItem('VITE_GITHUB_TOKEN');
+    const token = localStorage.getItem("VITE_GITHUB_TOKEN");
     if (!token) {
       dispatch(openSettings());
     } else {
       fetchRepos();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     dispatch(setReposInRedux(repos));
   }, [dispatch, repos]);
 
-  const filteredAndSortedRepos = useSelector((state: RootState) => selectSearchedRepos(state, selectedSort));
+  const filteredAndSortedRepos = useSelector((state: RootState) =>
+    selectSearchedRepos(state, selectedSort)
+  );
 
   const [satisfactory, unsatisfactory, optionalMissing] = useMemo(() => {
     let totalCheckboxes = 0;
     let totalXs = 0;
-    let totalOptionalMissing = 0
+    let totalOptionalMissing = 0;
     filteredAndSortedRepos.forEach((repo) => {
       if (repo.hasDevcontainer) {
         totalCheckboxes += 1;
@@ -53,28 +59,28 @@ function App() {
         totalOptionalMissing += 1;
       }
 
-      if(repo.hasDockerfile){
+      if (repo.hasDockerfile) {
         totalCheckboxes += 1;
       } else {
         totalOptionalMissing += 1;
       }
 
-      if(repo.hasReadme){
+      if (repo.hasReadme) {
         totalCheckboxes += 1;
       } else {
         totalXs += 1;
       }
 
-      if (repo.hasTodo){
-        totalCheckboxes+=1;
+      if (repo.hasTodo) {
+        totalCheckboxes += 1;
       } else {
-        totalXs +=1;
+        totalXs += 1;
       }
 
-      if(repo.license){
-        totalCheckboxes+=1;
+      if (repo.license) {
+        totalCheckboxes += 1;
       } else {
-        totalXs +=1
+        totalXs += 1;
       }
     });
     return [totalCheckboxes, totalXs, totalOptionalMissing];
@@ -103,15 +109,14 @@ function App() {
     <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
       {settingModalOpen && <SettingsModal />}
       <div className="max-w-7xl mx-auto">
-       <Header />
-       {loading && <div className='flex w-full justify-center'>
-        <Loader2Icon className='loading'/>
-       </div>
-      }
-
-        {reposRedux.length > 0 && (
-          <SearchAndSortContainer />
+        <Header />
+        {loading && (
+          <div className="flex w-full justify-center">
+            <Loader2Icon className="loading" />
+          </div>
         )}
+
+        {reposRedux.length > 0 && <SearchAndSortContainer />}
 
         {reposRedux.length > 0 && (
           <div className="flex flex-col items-center">
@@ -126,7 +131,9 @@ function App() {
               <span>Survey Results: </span>
               <span>{satisfactory} ✅</span>
               <span>{unsatisfactory} ❌</span>
-              <span className="flex">{optionalMissing} <ShieldQuestion/></span>
+              <span className="flex">
+                {optionalMissing} <ShieldQuestion />
+              </span>
             </div>
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
               {paginatedRepos.map((repo) => (
@@ -136,13 +143,15 @@ function App() {
             <div className="flex flex w-full justify-around">
               <p>Search Results: {filteredAndSortedRepos.length} Repos</p>
             </div>
-            {filteredAndSortedRepos.length > 0 && <Pager
-              totalEntries={filteredAndSortedRepos.length}
-              entriesPerPage={entriesPerPage}
-              currentPage={currentPage}
-              onEntriesPerPageChange={setEntriesPerPage}
-              onPageChange={setCurrentPage}
-            />}
+            {filteredAndSortedRepos.length > 0 && (
+              <Pager
+                totalEntries={filteredAndSortedRepos.length}
+                entriesPerPage={entriesPerPage}
+                currentPage={currentPage}
+                onEntriesPerPageChange={setEntriesPerPage}
+                onPageChange={setCurrentPage}
+              />
+            )}
           </div>
         )}
 
