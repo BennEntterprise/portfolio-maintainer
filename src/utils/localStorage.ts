917784b0969eb2424eec199089ba28f21d34c7a6
Kyle Bennett <kylebennett00001@gmail.com>
2025-01-19 23:13:44 -0500

    feat: add a get/set for ls method

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -2,3 +2,20 @@ export const LOCAL_STORAGE_KEYS = {
   FILTER_STATUS: 'FILTER_STATUS',
   VITE_GITHUB_TOKEN: 'VITE_GITHUB_TOKEN',
 }
+
+/**
+ * @description Will get a value from local storage. Assumes you will deserialize the value.
+ * @param key
+ * @returns string | null
+ */
+export const getLS = (key: string) => {
+  return localStorage.getItem(key)
+}
+/**
+ * @description Will set a value in local storage. Assumes you have already serialized the value.
+ * @param key
+ * @param value
+ */
+export const setLS = (key: string, value: string) => {
+  localStorage.setItem(key, value)
+}
