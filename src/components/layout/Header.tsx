8c95540b65490629fb654868f3fd28eb1aeb6477
Kyle Bennett <kylebennett00001@gmail.com>
2025-01-24 01:31:20 -0500

    fix: update site name Github Explorer -> Portfolio Maintainer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className="flex items-center justify-start mb-8">
       <div className="flex items-center justify-between w-full">
         <Github className="w-8 h-8 mr-3" />
-        <h1 className="text-3xl font-bold text-gray-900">GitHub Explorer</h1>
+        <h1 className="text-3xl font-bold text-gray-900">Portfolio Maintainer</h1>
         <SettingsCog onClick={() => dispatch(toggleSettings())} />
       </div>
     </header>
